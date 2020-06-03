7141cf66ebd61c83a8d9d21098efb82450062f96
Blake Sanders <blake.sanders10497@gmail.com>
2020-06-03 17:03:00 -0400

    Updated Drive and Intake subsystems to match SlippyBot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/Drive.java
@@ -2,37 +2,63 @@ package org.firstinspires.ftc.teamcode.robot.subsystems;
 
 import com.disnodeteam.dogecommander.Subsystem;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Drive implements Subsystem {
+
+    // Hardware map
     private HardwareMap hardwareMap;
 
-    private DcMotor leftMotor;
-    private DcMotor rightMotor;
+    // Components
+    private DcMotor flDrive;
+    private DcMotor frDrive;
+    private DcMotor rlDrive;
+    private DcMotor rrDrive;
 
+    // State and interface variables
     private double leftPower = 0;
     private double rightPower = 0;
 
+    // Subsystem constructor
     public Drive(HardwareMap hardwareMap) {
         this.hardwareMap = hardwareMap;
     }
 
+    // Subsystem control (called by commands)
     public void setPower(double leftPower, double rightPower) {
         this.leftPower = leftPower;
         this.rightPower = rightPower;
     }
 
+    // Initialization method ( similar to hardware.init(hardwareMap); )
     @Override
     public void initHardware() {
-        leftMotor = hardwareMap.get(DcMotor.class, "m1");
-        rightMotor = hardwareMap.get(DcMotor.class, "m2");
 
-        leftMotor.setDirection(DcMotor.Direction.REVERSE);
+        // Get drive motors
+        flDrive = hardwareMap.dcMotor.get("fl_drive");
+        frDrive = hardwareMap.dcMotor.get("fr_drive");
+        rlDrive = hardwareMap.dcMotor.get("rl_drive");
+        rrDrive = hardwareMap.dcMotor.get("rr_drive");
+
+        // Reverse left side
+        flDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+        rlDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        // Set all drive motors to brake on zero power
+        flDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rlDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rrDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
 
+    // Periodic method (called by DogeCommander)
     @Override
     public void periodic() {
-        leftMotor.setPower(leftPower);
-        rightMotor.setPower(rightPower);
+        // Set all drive motor powers from local variables
+        flDrive.setPower(leftPower);
+        frDrive.setPower(rightPower);
+        rlDrive.setPower(leftPower);
+        rrDrive.setPower(rightPower);
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/Intake.java
@@ -2,18 +2,27 @@ package org.firstinspires.ftc.teamcode.robot.subsystems;
 
 import com.disnodeteam.dogecommander.Subsystem;
 import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Intake implements Subsystem {
+
+    // Hardware map
     private HardwareMap hardwareMap;
 
-    private CRServo crServo;
+    // Components
+    private CRServo intakeLeft;
+    private CRServo intakeRight;
+
+    // State and interface variables
     private State state = State.STOP;
 
+    // Constructor
     public Intake(HardwareMap hardwareMap) {
         this.hardwareMap = hardwareMap;
     }
 
+    // State definition
     public enum State {
         INTAKE(1.0),
         SPIT_OUT(-1.0),
@@ -27,17 +36,26 @@ public class Intake implements Subsystem {
 
     }
 
+    // Subsystem control (called by commands)
     public void setState(State state) {
         this.state = state;
     }
 
+    // Subsystem initialization ( similar to hardware.init(hardwareMap) )
     @Override
     public void initHardware() {
-        this.crServo = hardwareMap.get(CRServo.class, "crservo");
+
+        // Get CR servos
+        intakeLeft  = hardwareMap.crservo.get("intake_left");
+        intakeRight = hardwareMap.crservo.get("intake_right");
+
+        // Reverse left side
+        intakeLeft.setDirection(DcMotorSimple.Direction.REVERSE);
     }
 
     @Override
     public void periodic() {
-        crServo.setPower(state.power);
+        intakeLeft. setPower(state.power);
+        intakeRight.setPower(state.power);
     }
 }
