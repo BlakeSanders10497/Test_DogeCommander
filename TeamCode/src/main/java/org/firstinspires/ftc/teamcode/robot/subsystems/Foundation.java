c5e6e8a8bcd520f4c55d4758ee6f2a3ab54e4375
Blake Sanders <blake.sanders10497@gmail.com>
2020-06-03 19:24:42 -0400

    Added foundation subsystem

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/Foundation.java
@@ -0,0 +1,58 @@
+package org.firstinspires.ftc.teamcode.robot.subsystems;
+
+import com.disnodeteam.dogecommander.Subsystem;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class Foundation implements Subsystem {
+
+    // Hardware map
+    private HardwareMap hardwareMap;
+
+    // Components
+    private Servo foundLeft;
+    private Servo foundRight;
+
+    // State and interface variables
+    private State state;
+
+    // Constructor
+    public Foundation(HardwareMap hardwareMap) {
+        this.hardwareMap = hardwareMap;
+    }
+
+    // State enum definitions
+    public enum State {
+        OPEN(1.0, 0.3),
+        GRIP(0.3, 1.0);
+
+        private final double leftPos, rightPos;
+
+        State(double leftPos, double rightPos) {
+            this.leftPos    = leftPos;
+            this.rightPos   = rightPos;
+        }
+    }
+
+    // Subsystem control (called by commands)
+    public void setState(State state) {
+        this.state = state;
+    }
+
+    // Subsystem initialization ( similar to hardware.init(hardwareMap) )
+    @Override
+    public void initHardware() {
+
+        // Get the foundation servos
+        foundLeft = hardwareMap.servo.get("found_left");
+        foundRight = hardwareMap.servo.get("found_right");
+    }
+
+
+    // Periodic method (called by DogeCommander)
+    @Override
+    public void periodic() {
+        foundLeft.  setPosition(state.leftPos);
+        foundRight. setPosition(state.rightPos);
+    }
+}
