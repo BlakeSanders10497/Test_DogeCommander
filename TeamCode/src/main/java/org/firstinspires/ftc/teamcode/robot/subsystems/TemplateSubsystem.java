95f915de0fd8bb00a827016a90b2c92d50969a36
Blake Sanders <blake.sanders10497@gmail.com>
2020-06-03 17:37:49 -0400

    Added subsystem template class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/TemplateSubsystem.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode.robot.subsystems;
+
+import com.disnodeteam.dogecommander.Subsystem;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class TemplateSubsystem implements Subsystem {
+
+    // Hardware map
+    private HardwareMap hardwareMap;
+
+    // Components
+
+    // State and interface variables
+
+    // Constructor
+    public TemplateSubsystem(HardwareMap hardwareMap) {
+        this.hardwareMap = hardwareMap;
+    }
+
+    // Subsystem control (called by commands)
+
+    // Subsystem initialization ( similar to hardware.init(hardwareMap) )
+    @Override
+    public void initHardware() {
+
+    }
+
+
+    // Periodic method (called by DogeCommander)
+    @Override
+    public void periodic() {
+    }
+}
