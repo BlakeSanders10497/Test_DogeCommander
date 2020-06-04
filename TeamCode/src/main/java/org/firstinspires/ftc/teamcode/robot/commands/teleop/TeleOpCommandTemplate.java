599c2f9208309a641f2c3bef820cab0ef6185983
Blake Sanders <blake.sanders10497@gmail.com>
2020-06-03 20:15:02 -0400

    Added TeleOp command template

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/commands/teleop/TeleOpCommandTemplate.java
@@ -0,0 +1,50 @@
+package org.firstinspires.ftc.teamcode.robot.commands.teleop;
+
+import com.disnodeteam.dogecommander.Command;
+import com.disnodeteam.dogecommander.Subsystem;
+import com.qualcomm.robotcore.hardware.Gamepad;
+
+import org.firstinspires.ftc.teamcode.robot.subsystems.Drive;
+import org.slf4j.helpers.SubstituteLogger;
+
+public class TeleOpCommandTemplate implements Command {
+
+
+    // Subsystem
+    private Subsystem subsystem;
+
+    // Input
+    private Gamepad gamepad;
+
+    // Output variables
+
+    // Constructor
+    public TeleOpCommandTemplate(Subsystem subsystem, Gamepad gamepad) {
+        this.subsystem = subsystem;
+        this.gamepad = gamepad;
+    }
+
+    // Initial state
+    @Override
+    public void start() {
+
+    }
+
+    // Running state
+    @Override
+    public void periodic() {
+
+    }
+
+    // End state
+    @Override
+    public void stop() {
+
+    }
+
+    // Progress tracking
+    @Override
+    public boolean isCompleted() {
+        return false;
+    }
+}
