5da45ad94280bd1a9659a9cb9d9faba6c7b63142
Blake Sanders <blake.sanders10497@gmail.com>
2020-06-03 20:36:28 -0400

    Added arm controls

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/commands/teleop/TeleOpArmControl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/commands/teleop/TeleOpArmControl.java
@@ -4,38 +4,44 @@ import com.disnodeteam.dogecommander.Command;
 import com.disnodeteam.dogecommander.Subsystem;
 import com.qualcomm.robotcore.hardware.Gamepad;
 
+import org.firstinspires.ftc.teamcode.robot.subsystems.Arm;
+
 public class TeleOpArmControl implements Command {
-    
+
     // Subsystem
-    private Subsystem subsystem;
+    private Arm arm;
 
     // Input
     private Gamepad gamepad;
 
     // Output variables
+    private double armPower;
 
     // Constructor
-    public TeleOpArmControl(Subsystem subsystem, Gamepad gamepad) {
-        this.subsystem = subsystem;
+    public TeleOpArmControl(Arm arm, Gamepad gamepad) {
+        this.arm = arm;
         this.gamepad = gamepad;
     }
 
     // Initial state
     @Override
     public void start() {
-
+        arm.setArmPower(0.0);
     }
 
     // Running state
     @Override
     public void periodic() {
 
+        // Drive arm
+        armPower = gamepad.left_stick_y;
+        arm.setArmPower(armPower);
     }
 
     // End state
     @Override
     public void stop() {
-
+        arm.setArmPower(0.0);
     }
 
     // Progress tracking
