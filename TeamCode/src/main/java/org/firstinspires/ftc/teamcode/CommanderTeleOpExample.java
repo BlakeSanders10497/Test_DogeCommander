49f01aa0594ac7b6c4e1be978ba0cd200ebdc70c
Blake Sanders <blake.sanders10497@gmail.com>
2020-06-03 20:49:18 -0400

    Added arm control

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CommanderTeleOpExample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CommanderTeleOpExample.java
@@ -5,8 +5,10 @@ import com.disnodeteam.dogecommander.DogeOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.teamcode.robot.commands.teleop.TeleOpArmControl;
 import org.firstinspires.ftc.teamcode.robot.commands.teleop.TeleOpDriveControl;
 import org.firstinspires.ftc.teamcode.robot.commands.teleop.TeleOpIntakeControl;
+import org.firstinspires.ftc.teamcode.robot.subsystems.Arm;
 import org.firstinspires.ftc.teamcode.robot.subsystems.Drive;
 import org.firstinspires.ftc.teamcode.robot.subsystems.Intake;
 
@@ -16,18 +18,21 @@ public class CommanderTeleOpExample extends LinearOpMode implements DogeOpMode {
     public void runOpMode() {
         DogeCommander commander = new DogeCommander(this);
 
-        Drive drive = new Drive(hardwareMap);
-        Intake intake = new Intake(hardwareMap);
+        Drive drive     = new Drive (hardwareMap);
+        Intake intake   = new Intake(hardwareMap);
+        Arm arm         = new Arm   (hardwareMap);
 
         commander.registerSubsystem(drive);
         commander.registerSubsystem(intake);
+        commander.registerSubsystem(arm);
         commander.init();
 
         waitForStart();
 
         commander.runCommandsParallel(
-                new TeleOpDriveControl(drive, gamepad1),
-                new TeleOpIntakeControl(intake, gamepad1)
+                new TeleOpDriveControl  (drive,     gamepad1),
+                new TeleOpIntakeControl (intake,    gamepad2),
+                new TeleOpArmControl    (arm,       gamepad2)
         );
 
         commander.stop();
