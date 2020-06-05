2e0eb89ce1beeae5dac73ac99a6d33c04b2e3c1d
Blake Sanders <blake.sanders10497@gmail.com>
2020-06-05 01:34:33 -0400

    Added gripper release stone command

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CommanderAutonomousExample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CommanderAutonomousExample.java
@@ -10,6 +10,7 @@ import org.firstinspires.ftc.teamcode.robot.commands.auto.DriveByEncoder;
 import org.firstinspires.ftc.teamcode.robot.commands.auto.GripperGripStone;
 import org.firstinspires.ftc.teamcode.robot.commands.auto.GripperPrepGripStone;
 import org.firstinspires.ftc.teamcode.robot.commands.auto.GripperPrepReleaseStone;
+import org.firstinspires.ftc.teamcode.robot.commands.auto.GripperReleaseStone;
 import org.firstinspires.ftc.teamcode.robot.commands.auto.RunIntakeForTime;
 import org.firstinspires.ftc.teamcode.robot.subsystems.Arm;
 import org.firstinspires.ftc.teamcode.robot.subsystems.Drive;
@@ -46,6 +47,7 @@ public class CommanderAutonomousExample extends LinearOpMode implements DogeOpMo
         commander.runCommand(new GripperGripStone(gripper, 0.0));
         commander.runCommand(new ArmByEncoder(arm, -2400, 0.5, 3.0));
         commander.runCommand(new GripperPrepReleaseStone(gripper, 0.0));
+        commander.runCommand(new GripperReleaseStone(gripper, 0.0));
 
         commander.stop();
     }
